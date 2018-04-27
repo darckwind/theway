62263709721df808723c1e567e2237e844f0ad02
darckwind <f.bunout01@ufromail.cl>
2018-04-27 10:02:08 -0300

    se agrego la ventana selectora de tours

--- src/main/java/UI/SelectTour.java
+++ src/main/java/UI/SelectTour.java
@@ -1,4 +1,28 @@
 package UI;
 
-public class SelectTour {
+import javax.swing.*;
+import java.awt.*;
+
+public class SelectTour extends JFrame {
+
+    private int alto=200;
+    private int ancho=400;
+    private String titulo ="Tour's";
+    private JButton agregar;
+    private JButton actualizar;
+    private JComboBox tour;
+
+    public SelectTour(){
+
+    }
+    private void Orden(){
+        setLayout(new BorderLayout());
+        setTitle(titulo);
+        setSize(ancho, alto);
+        setMinimumSize(getSize());
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
 }
